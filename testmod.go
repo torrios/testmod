407d3d63c87d47d7e3a29a4b7adf3c9fe662fd15
Hector Rios <oj.rios@gmail.com>
2018-09-19 14:04:18 +0200

    forgot to return error in most scenarios

--- testmod.go
+++ testmod.go
@@ -9,13 +9,13 @@ import (
 func Hi(name string, lang string) (string, error) {
 	switch lang {
 	case "en":
-		return fmt.Sprintf("Hi %s!", name)
+		return fmt.Sprintf("Hi %s!", name), nil
 	case "pt":
-		return fmt.Sprintf("Oi  %s!", name)
+		return fmt.Sprintf("Oi  %s!", name), nil
 	case "es":
-		return fmt.Sprintf("Hola %s!", name)
+		return fmt.Sprintf("Hola %s!", name), nil
 	case "fr":
-		return fmt.Sprintf("Bonjour %s!", name)
+		return fmt.Sprintf("Bonjour %s!", name), nil
 	default:
 		return "", errors.New("unknown Language")
 	}
